74eb49a270f014e051152af8ad6a9275d7ed6f81
Laimis <laimis@pc>
2023-11-26 18:10:59 +0200

    array challenge

--- Program.cs
+++ Program.cs
@@ -1,23 +1,11 @@
-﻿Random random = new Random();
-int daysUntilExpiration = random.Next(12);
-int discountPercentage = 0;
+﻿//array challenges
 
-// Your code goes here
+string[] array = {"B123","C234","A345","C15","B177","G3003","C235","B179"};
 
-Console.WriteLine($"Days left: {daysUntilExpiration}");
-
-if(daysUntilExpiration==0){
-    Console.WriteLine("Your subscription has expired.");
-}else if(daysUntilExpiration==1){
-    discountPercentage = 20;
-    Console.WriteLine("Your subscription expires within a day!");
-    Console.WriteLine($"Renew now and save {discountPercentage}%!");
-}else if(daysUntilExpiration<=5){
-    discountPercentage = 10;
-    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days!");
-    Console.WriteLine($"Renew now and save {discountPercentage}%!");
-}else if(daysUntilExpiration<=10){
-    Console.WriteLine("Your subscription will expire soon. Renew now!");
-}else{
-    Console.WriteLine("All good!");
+foreach(string el in array){
+    if(el.StartsWith('B')){
+            Console.WriteLine($"{el} starts with 'B'!");
+    }
 }
+
+Console.WriteLine();
