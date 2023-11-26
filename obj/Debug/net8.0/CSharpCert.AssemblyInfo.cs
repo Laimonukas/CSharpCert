79ef87db9aa523909115f8d3f0418e1d1dfcf72f
Laimis <laimis@pc>
2023-11-26 15:35:13 +0200

    CodeChallenge

--- obj/Debug/net8.0/CSharpCert.AssemblyInfo.cs
+++ obj/Debug/net8.0/CSharpCert.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharpCert")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57ed44f27901e42cf8a42f87e642da44a0e09b9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharpCert")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharpCert")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
