2108bb8391847887e7c03639d3b9049e4d702a5f
Laimis <laimis@pc>
2023-11-26 16:05:50 +0200

    updated challenge

--- Program.cs
+++ Program.cs
@@ -4,17 +4,19 @@ int discountPercentage = 0;
 
 // Your code goes here
 
+Console.WriteLine($"Days left: {daysUntilExpiration}");
+
 if(daysUntilExpiration==0){
     Console.WriteLine("Your subscription has expired.");
 }else if(daysUntilExpiration==1){
     discountPercentage = 20;
     Console.WriteLine("Your subscription expires within a day!");
     Console.WriteLine($"Renew now and save {discountPercentage}%!");
-}else if(daysUntilExpiration<5){
+}else if(daysUntilExpiration<=5){
     discountPercentage = 10;
     Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days!");
     Console.WriteLine($"Renew now and save {discountPercentage}%!");
-}else if(daysUntilExpiration<10){
+}else if(daysUntilExpiration<=10){
     Console.WriteLine("Your subscription will expire soon. Renew now!");
 }else{
     Console.WriteLine("All good!");
