3118e6c494e246e29913b47ef107ec69bbc14ee1
Laimis <laimisjakas@gmail.com>
2023-11-29 16:01:13 +0200

    Evaluation challenge

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿Random coinFlip = new Random();
-Console.WriteLine(coinFlip.Next(0,2) == 0 ? "heads":"tails");
+﻿string permission = "Admi|Manager";
+int level = 25;
+
+var response = permission.Contains("Admin")  ? (level > 55 ? "Welcome, Super Admin user." : "Welcome Admin User") : (permission.Contains("Manager") ? (level >= 20 ? "Contact an Admin for access." : "You do not have sufficient privileges.") : "You do not have sufficient privileges.");
+
+Console.WriteLine(response);
