6d94be7a6471c114ceb73012d14b81173aab1655
Laimis <laimisjakas@gmail.com>
2023-11-30 19:01:38 +0200

    fizzbuzz challenge

--- Program.cs
+++ Program.cs
@@ -1,53 +1,5 @@
-﻿// SKU = Stock Keeping Unit. 
-// SKU value format: <product #>-<2-letter color code>-<size code>
-string sku = "01-MN-L";
+﻿// FizzBuzz challenge
 
-string[] product = sku.Split('-');
-
-string type = "";
-string color = "";
-string size = "";
-
-switch(product[0]){
-    case "01":
-        type = "Sweat shirt";
-        break;
-    case "02":
-        type = "T-Shirt";
-        break;
-    case "03":
-        type = "Sweat pants";
-        break;
-    default:
-        type = "Other";
-        break;
+for (int i = 1;i < 100; i++){
+    Console.WriteLine($"{i}{(i % 3 == 0 ? (i % 5 == 0 ? " - FizzBuzz":" - Fizz") : (i % 5 == 0 ? " - Buzz":""))}");
 }
-
-switch(product[1]){
-    case "BL":
-        color = "Black";
-        break;
-    case "MN":
-        color = "Maroon";
-        break;
-    default :
-        color = "White";
-        break;
-}
-
-switch(product[2]){
-    case "S":
-        size = "Small";
-        break;
-    case "M":
-        size = "Medium";
-        break;
-    case "L":
-        size = "Large";
-        break;
-    default :
-        color = "One Size Fits All";
-        break;
-}
-
-Console.WriteLine($"Product: {size} {color} {type}");
