cccf1a8f550788adcd8e93029c40add6f57019e4
Laimis <laimisjakas@gmail.com>
2023-12-05 00:14:01 +0200

    Code project 2 - write code that validates string

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ Your solution must ensure that the input is a valid representation of an integer
 If the integer value isn't between 5 and 10, your code must use a Console.WriteLine() statement to prompt the user for an integer value between 5 and 10.
 Your solution must ensure that the integer value is between 5 and 10 before exiting the iteration.
 Below (after) the iteration code block: your solution must use a Console.WriteLine() statement to inform the user that their input value has been accepted.
-*/
+
 #nullable disable
 
 Console.WriteLine("Input int value between 5 and 10");
@@ -36,4 +36,43 @@ do
 } while (!validInput);
 
 Console.WriteLine(response);
+*/
 
+
+/*
+Code project 2 - write code that validates string input
+Here are the conditions that your second coding project must implement:
+
+Your solution must include either a do-while or while iteration.
+
+Before the iteration block: your solution must use a Console.WriteLine() statement to prompt the user for one of three role names: Administrator, Manager, or User.
+
+Inside the iteration block:
+
+Your solution must use a Console.ReadLine() statement to obtain input from the user.
+Your solution must ensure that the value entered matches one of the three role options.
+Your solution should use the Trim() method on the input value to ignore leading and training space characters.
+Your solution should use the ToLower() method on the input value to ignore case.
+If the value entered isn't a match for one of the role options, your code must use a Console.WriteLine() statement to prompt the user for a valid entry.
+Below (after) the iteration code block: Your solution must use a Console.WriteLine() statement to inform the user that their input value has been accepted.
+*/
+
+#nullable disable
+string[] roles = ["administrator","manager","user"];
+var validInput = false;
+string consoleInput = "Default";
+Console.WriteLine("Enter role name, (Administrator, Manager, User)");
+
+do
+{
+    consoleInput = Console.ReadLine().Trim().ToLower();
+    if (roles.Contains(consoleInput))
+    {
+        validInput = true;
+        break;
+    }
+
+    Console.WriteLine($"The role name that you entered, {consoleInput} is not valid. Enter your role name (Administrator, Manager, or User)");
+} while (!validInput);
+
+Console.WriteLine($"Your input value ({consoleInput}) has been accepted.");
